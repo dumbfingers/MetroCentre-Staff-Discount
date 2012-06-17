4f86edd3d01b65094839de59b4764d2b949e335e
ss1271 <ss1271@gmail.com>
2012-06-17 18:01:44 +0100

    Initial Import

--- src/com/yeyaxi/MetroCentreStaffDiscount/MetroCentreStaffDiscountActivity.java
+++ src/com/yeyaxi/MetroCentreStaffDiscount/MetroCentreStaffDiscountActivity.java
@@ -4,10 +4,35 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class MetroCentreStaffDiscountActivity extends Activity {
+	private DataBaseHelper mDataBase;
+	private int _id;
+	private String _ShopName;
+	private double _Discount;
+	private String _Note;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mDataBase = new DataBaseHelper(getBaseContext()); 
         setContentView(R.layout.main);
+    }
+    
+    /**
+     * getId
+     * @return
+     */
+    public int getId() {
+    	
+    	return _id;
+    }
+    
+    /**
+     * getShopName
+     * @param Id
+     * @return
+     */
+    public String getShopName(int Id) {
+    	
+    	return _ShopName;
     }
 }
